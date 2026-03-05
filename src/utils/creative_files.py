cabf3c73b356cc7c1f9bbb18a3b775476ddc59a9
sk1lizz <salimgareewruslan@gmail.com>
2026-03-05 19:04:30 +0300

    init

---
+++ src/utils/creative_files.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+
+class creative_files:
+
+    __name: str # 
+    __dir_setting: str #
+    __dir_common: str # 
+    __dir_lang: str #
+
+    __file_setting: str #
+    __file_history: str # 
+
+    def __init__(self) -> None:
+        pass
