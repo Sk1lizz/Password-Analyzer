84993932c4b3bf1a5c26b09b22157f5840c867c6
sk1lizz <salimgareewruslan@gmail.com>
2026-03-06 16:19:03 +0300

    edit create_file( add get_path), add edit_data

---
+++ src/utils/edit_data.py
@@ -0,0 +1,85 @@
+import json
+
+"""
+
+"""
+
+class edit_data:
+
+    path_to_config: str
+    path_to_history: str
+    path_to_language: str
+
+    def __init__(self, dict_path: dict | None = None) -> None:
+        self.path_to_config = dict_path["setting"]
+        self.path_to_history = dict_path["history"]
+        path_lang = dict_path["language"]
+
+        try:
+            with open(self.path_to_config, mode="r", encoding="utf-8") as __file:
+                __data = json.load(__file)
+            
+            self.path_to_language = f"{path_lang}/{__data['language']}"
+        
+        except:
+            self.path_to_language = f"C:/Users/salim/AppData/Roaming/PasswordAnalyzer/language/ru-RU.json"
+
+        return
+    
+    def get_config(self, arg: str | None = None) -> str | int | None:
+        if arg is None:
+            return None
+        
+        try:
+            with open(self.path_to_config, mode="r", encoding="utf-8") as __file:
+                __data = json.load(__file)
+        except:
+            return None
+        
+        if not arg in __data.keys():
+            return False
+        
+        return __data[arg]
+    
+    def get_history(self, amount_history: int = 5) -> list:
+
+        result = list()
+        
+        try:
+            with open(self.path_to_history, mode="r", encoding="utf-8") as __file:
+                __data = __file.read()
+        except:
+            return None
+
+        amount = 1
+
+        for lines in __data.split("\n"):
+            if amount > amount_history:
+                break
+            
+            result.append(lines)
+            amount += 1
+        
+        return result
+
+    def add_history(self, message: str | None) -> bool:
+        if message is None:
+            return False
+        
+
+        try:
+            with open(self.path_to_history, mode="r", encoding="utf-8") as __file:
+                __data = __file.read()
+        except:
+            return False
+        
+
+        text = f"{message}\n{__data}"
+        
+        try:
+            with open(self.path_to_history, mode="w", encoding="utf-8") as __file:
+                __file.write(text)
+
+            return True
+        except:
+            return False
--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import src.models.config as config
+import src.utils.create_files as cr
+import src.utils.edit_data as gd
 
 test_config = config.config()
 
-print(test_config.get_config_path())
+config_dict = test_config.get_config_path()
 
+test_cr = cr.create_files(config_dict=config_dict)
+
+
+print(test_cr.create_file())
+
+dict_cr = test_cr.get_path()
+
+print(dict_cr)
+
+test_gd = gd.edit_data(dict_path=dict_cr)
+print(test_gd.add_history("Hello, World!6"))
+print(test_gd.get_history(5))
