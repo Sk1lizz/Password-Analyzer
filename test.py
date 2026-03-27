402af695c51494f838d889b67b42553409a6546e
sk1lizz <salimgareewruslan@gmail.com>
2026-03-27 21:57:11 +0300

    add new bugs 😂😂😂😂

--- test.py
+++ test.py
@@ -1,6 +1,24 @@
 import src.views.main_window as window
-
+import src.utils.create_files as create_file
+import src.models.config as cfg
 
 if __name__ == "__main__":
+    test_cfg = cfg.config()
+
+    path = test_cfg.get_config_path()
+
+    test_cr = create_file.create_files(config_dict=path)
+
+    test_cr.create_file()
+
+    path = test_cr.get_path()
+
     main = window.setting()
+
+    main.set_config(path)
+    main.start()
+
+
+
+    
 
